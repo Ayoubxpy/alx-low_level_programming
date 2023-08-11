a6ad0548a416e2aa5df987da4608fd9c03a4b5cf
Ayoub Loudini <ayoubld2018@outlook.com>
2023-08-11 14:23:10 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * Description:prints the numbers
+ * Return:0(success)
+*/
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; number++)
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+	return (0);
+}
