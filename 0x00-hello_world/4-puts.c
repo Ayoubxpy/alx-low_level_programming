b4ec2b3b51136ad3fa7a2772094a1b778daf709e
Ayoub Loudini <ayoubld2018@outlook.com>
2023-08-10 20:01:36 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - IS THE ENTRY POINT OF THE CODE
+ *
+ * description :SECOND LINE PRINT THE LINE WITH PUTS FUNCTION
+ *
+ * RETURN : 0 THAT MEAN SUCCESS
+ * */
+int main(void) 
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
