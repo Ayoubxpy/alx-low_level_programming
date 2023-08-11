05e4bd223450846019021a823343db26794b58d8
Ayoub Loudini <ayoubld2018@outlook.com>
2023-08-11 08:07:53 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - main entry point
- * description : puts funnction print the txt
- * return : Always 0 (success)
- */
+ *main - main entry point
+ *description:puts function print the line
+ *Return: 0 (success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
