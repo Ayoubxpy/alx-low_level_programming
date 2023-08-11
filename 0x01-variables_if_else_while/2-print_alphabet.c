8b002bc63e05736b7ef23ae8b3a6a2c32f4218ca
Ayoub Loudini <ayoubld2018@outlook.com>
2023-08-11 13:53:34 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * Description:prints the alphabet in lowercase
+ * Return:0(success)
+*/
+
+int main(void)
+{
+	int numberl = 97;
+
+	while (numberl <= 122)
+	{
+		putchar(numberl);
+		numberl++;
+	}
+	return (0);
+}
