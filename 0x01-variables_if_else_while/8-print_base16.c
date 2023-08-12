d8a61cafdc5dc7a2ade0bc9548ff71189c2ca7eb
Ayoub Loudini <ayoubld2018@outlook.com>
2023-08-11 18:29:03 -0700

    print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ * Description:prints the alphabet in lowercase + numbers
+ * Return:0(success)
+*/
+
+int main(void)
+{
+	int number;
+	int alp;
+
+	for (number = 48; number <= 57; number++)
+	{
+		putchar(number);
+	}
+	for (alp = 97; alp <= 100; alp++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
